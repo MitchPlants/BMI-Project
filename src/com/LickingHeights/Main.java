c43e5a80de5d098da8d52b0f60f0582bb228507b
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-17 09:37:33 -0400

    Asking for Name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,8 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner Keyboard;
         Keyboard = new Scanner(System.in);
+        String Name;
         int Weight,Height;
+        System.out.println("Hello, We are going to find your BMI. What is your name?");
+        Name = Keyboard.nextLine();
         
+
+
+
 
 
 
