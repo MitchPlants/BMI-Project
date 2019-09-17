e3cb2c9619bc69200d0d294bb2be4dce44b0360a
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-17 09:33:43 -0400

    Added Scanner and Integer Variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,9 +1,16 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
+        int Weight,Height;
         
-	// write your code here
+
+
+
     }
 }
